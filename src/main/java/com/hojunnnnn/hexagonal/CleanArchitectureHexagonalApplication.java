465b872ebf05ba74f8ed90ad39dd1318b1c6a39f
HJLee <gotjd8515@gmail.com>
2025-09-16 14:14:13 +0900

    Initial commit

---
+++ src/main/java/com/hojunnnnn/hexagonal/CleanArchitectureHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.hojunnnnn.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureHexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanArchitectureHexagonalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hojunnnnn/hexagonal/CleanArchitectureHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hojunnnnn.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureHexagonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
