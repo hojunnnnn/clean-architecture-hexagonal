442307325aed63421429534cab67688621d5ae51
HJLee <gotjd8515@gmail.com>
2025-09-16 16:27:04 +0900

    비즈니스 규칙 검증하기

--- src/main/java/com/hojunnnnn/hexagonal/account/application/service/SendMoneyService.java
+++ src/main/java/com/hojunnnnn/hexagonal/account/application/service/SendMoneyService.java
@@ -2,14 +2,36 @@ package com.hojunnnnn.hexagonal.account.application.service;
 
 import com.hojunnnnn.hexagonal.account.application.port.in.SendMoneyCommand;
 import com.hojunnnnn.hexagonal.account.application.port.in.SendMoneyUseCase;
+import com.hojunnnnn.hexagonal.account.application.port.out.AccountLock;
+import com.hojunnnnn.hexagonal.account.application.port.out.LoadAccountPort;
+import com.hojunnnnn.hexagonal.account.application.port.out.UpdateAccountStatePort;
+import com.hojunnnnn.hexagonal.account.domain.Account;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
 public class SendMoneyService implements SendMoneyUseCase {
+
+    private final LoadAccountPort loadAccountPort;
+    private final AccountLock accountLock;
+    private final UpdateAccountStatePort updateAccountStatePort;
 
     @Override
     public boolean sendMoney(SendMoneyCommand command) {
+        requireAccountExists(command.getSourceAccountId());
+        requireAccountExists(command.getTargetAccountId());
         // TODO: 비즈니스 규칙 검증
         // TODO: 모델 상태 조작
         // TODO: 출력 값 반환
         return true;
+    }
+
+    private void requireAccountExists(Account.AccountId targetAccountId) {
+        Account account = loadAccountPort.loadAccount(targetAccountId, LocalDateTime.now());
+        if (account == null) {
+            throw new IllegalArgumentException("Account does not exist");
+        }
     }
 }
