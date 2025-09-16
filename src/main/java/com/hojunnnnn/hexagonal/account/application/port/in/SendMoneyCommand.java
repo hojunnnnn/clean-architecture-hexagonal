50a70f502fdf96a3ef6a53a6dc7788b3604b4c02
HJLee <gotjd8515@gmail.com>
2025-09-16 16:03:41 +0900

    입력 유효성 검증

--- src/main/java/com/hojunnnnn/hexagonal/account/application/port/in/SendMoneyCommand.java
+++ src/main/java/com/hojunnnnn/hexagonal/account/application/port/in/SendMoneyCommand.java
@@ -2,15 +2,29 @@ package com.hojunnnnn.hexagonal.account.application.port.in;
 
 import com.hojunnnnn.hexagonal.account.domain.Account;
 import com.hojunnnnn.hexagonal.account.domain.Money;
+import com.hojunnnnn.hexagonal.common.SelfValidating;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
-public class SendMoneyCommand {
+public class SendMoneyCommand extends SelfValidating<SendMoneyCommand> {
 
+    @NotNull
     private final Account.AccountId sourceAccountId;
+
+    @NotNull
     private final Account.AccountId targetAccountId;
+
+    @NotNull
     private final Money money;
 
+    public SendMoneyCommand(
+        Account.AccountId sourceAccountId,
+        Account.AccountId targetAccountId,
+        Money money) {
+        this.sourceAccountId = sourceAccountId;
+        this.targetAccountId = targetAccountId;
+        this.money = money;
+        this.validateSelf();
+    }
 }
